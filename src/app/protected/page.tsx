43f0f736c949e3e7d6ecb7a35089d117d47f9ee9
hi mochizuki <hi.mochizuki@buysell-technologies.com>
2025-01-04 17:28:23 +0900

    style: format

--- src/app/protected/page.tsx
+++ src/app/protected/page.tsx
@@ -4,10 +4,11 @@ import { CreateTravelDialog } from "./createTravelDialog";
 import TravelCard from "./travelCard";
 
 export default async function ProtectedPage() {
-	const supabase = await createClient();
-	const { data: travels } = await supabase
-  .from("travels")
-  .select(`
+  const supabase = await createClient();
+  const { data: travels } = await supabase
+    .from("travels")
+    .select(
+      `
     travel_id,
     travel_name,
     created_by,
@@ -15,28 +16,31 @@ export default async function ProtectedPage() {
       user_id,
       role
     )
-  `)
-  .order("created_at", { ascending: false });
-	const {
-		data: { user },
-	} = await supabase.auth.getUser();
+  `
+    )
+    .order("created_at", { ascending: false });
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
 
-	console.log(travels);
+  if (!user) {
+    return redirect("/sign-in");
+  }
 
-	if (!user) {
-		return redirect("/sign-in");
-	}
-
-	return (
-		<div className="flex-1 w-full flex flex-col gap-12">
-			<CreateTravelDialog />
-			<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-				{travels?.map((travel) => (
-					<TravelCard key={travel.travel_id} travelId={travel.travel_id} travelName={travel.travel_name} createdBy={travel.created_by}
-					travelMembers={travel.travel_members}
-					/>
-				))}
-			</div>
-		</div>
-	);
+  return (
+    <div className="flex-1 w-full flex flex-col gap-12">
+      <CreateTravelDialog />
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+        {travels?.map((travel) => (
+          <TravelCard
+            key={travel.travel_id}
+            travelId={travel.travel_id}
+            travelName={travel.travel_name}
+            createdBy={travel.created_by}
+            travelMembers={travel.travel_members}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
--- src/app/protected/travelCard.tsx
+++ src/app/protected/travelCard.tsx
@@ -6,10 +6,15 @@ type Props = {
   travelId: string;
   travelName: string;
   createdBy: string;
-  travelMembers: { user_id: string;}[];
-}
+  travelMembers: { user_id: string }[];
+};
 
-export default function TravelCard({ travelId, travelName, createdBy, travelMembers }: Props) {
+export default function TravelCard({
+  travelId,
+  travelName,
+  createdBy,
+  travelMembers,
+}: Props) {
   const router = useRouter();
 
   const handleClick = () => {
@@ -17,7 +22,11 @@ export default function TravelCard({ travelId, travelName, createdBy, travelMemb
   };
 
   return (
-    <button onClick={handleClick} type="button" className="bg-white shadow-md rounded-lg p-4 cursor-pointer">
+    <button
+      onClick={handleClick}
+      type="button"
+      className="bg-white shadow-md rounded-lg p-4 cursor-pointer"
+    >
       <h2 className="text-xl font-bold mb-2">{travelName}</h2>
       {/* TODO: 名前を表示する */}
       <p className="text-gray-600">作成者: {createdBy}</p>
