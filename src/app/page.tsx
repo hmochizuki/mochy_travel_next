96d83670a3b8f61cf034f203ee5fa55a76b43553
hi mochizuki <hi.mochizuki@buysell-technologies.com>
2025-01-04 12:37:35 +0900

    feat: Topページを変更

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
-import Hero from "@/components/hero";
-import SignUpUserSteps from "@/components/tutorial/sign-up-user-steps";
-
 export default async function Home() {
   return (
     <>
-      <Hero />
       <main className="flex-1 flex flex-col gap-6 px-4">
-        <h2 className="font-medium text-xl mb-4">Next steps</h2>
-        <SignUpUserSteps />
+        <div className="relative">
+        <h2 className="text-2xl font-bold">mochy travel</h2>
+        <p className="text-sm text-muted-foreground">
+          旅のしおりを作るアプリ
+        </p>
+      </div>
       </main>
     </>
   );
