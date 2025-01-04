be86b2f7a897fe7600c483c3348d174566d070e1
hi mochizuki <hi.mochizuki@buysell-technologies.com>
2025-01-04 12:41:55 +0900

    style: format by biome

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,51 +6,51 @@ import Link from "next/link";
 import "./globals.css";
 
 const defaultUrl = process.env.VERCEL_URL
-  ? `https://${process.env.VERCEL_URL}`
-  : "http://localhost:3000";
+	? `https://${process.env.VERCEL_URL}`
+	: "http://localhost:3000";
 
 export const metadata = {
-  metadataBase: new URL(defaultUrl),
-  title: "mochy travel",
-  description: "旅のしおりを作るアプリ",
+	metadataBase: new URL(defaultUrl),
+	title: "mochy travel",
+	description: "旅のしおりを作るアプリ",
 };
 
 const geistSans = Geist({
-  display: "swap",
-  subsets: ["latin"],
+	display: "swap",
+	subsets: ["latin"],
 });
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en" className={geistSans.className} suppressHydrationWarning>
-      <body className="bg-background text-foreground">
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          <main className="min-h-screen flex flex-col items-center">
-            <div className="flex-1 w-full flex flex-col gap-20 items-center">
-              <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-                <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
-                  <div className="flex gap-5 items-center font-semibold">
-                    <Link href={"/"}>mochy travel</Link>
-                  </div>
-                  <HeaderAuth />
-                </div>
-              </nav>
-              <div className="flex flex-col gap-20 max-w-5xl p-5">
-                {children}
-              </div>
-            </div>
-          </main>
-        </ThemeProvider>
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en" className={geistSans.className} suppressHydrationWarning>
+			<body className="bg-background text-foreground">
+				<ThemeProvider
+					attribute="class"
+					defaultTheme="system"
+					enableSystem
+					disableTransitionOnChange
+				>
+					<main className="min-h-screen flex flex-col items-center">
+						<div className="flex-1 w-full flex flex-col gap-20 items-center">
+							<nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
+								<div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
+									<div className="flex gap-5 items-center font-semibold">
+										<Link href={"/"}>mochy travel</Link>
+									</div>
+									<HeaderAuth />
+								</div>
+							</nav>
+							<div className="flex flex-col gap-20 max-w-5xl p-5">
+								{children}
+							</div>
+						</div>
+					</main>
+				</ThemeProvider>
+			</body>
+		</html>
+	);
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
 export default async function Home() {
-  return (
-    <>
-      <main className="flex-1 flex flex-col gap-6 px-4">
-        <div className="relative">
-        <h2 className="text-2xl font-bold">mochy travel</h2>
-        <p className="text-sm text-muted-foreground">
-          旅のしおりを作るアプリ
-        </p>
-      </div>
-      </main>
-    </>
-  );
+	return (
+		<>
+			<main className="flex-1 flex flex-col gap-6 px-4">
+				<div className="relative">
+					<h2 className="text-2xl font-bold">mochy travel</h2>
+					<p className="text-sm text-muted-foreground">
+						旅のしおりを作るアプリ
+					</p>
+				</div>
+			</main>
+		</>
+	);
 }
