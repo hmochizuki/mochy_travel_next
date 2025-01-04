ca7679bedc25d5a8e24af425c36a88bbfab23d8a
hi mochizuki <hi.mochizuki@buysell-technologies.com>
2025-01-04 17:27:47 +0900

    feat: 参加者人数を表示

--- src/app/protected/page.tsx
+++ src/app/protected/page.tsx
@@ -5,11 +5,23 @@ import TravelCard from "./travelCard";
 
 export default async function ProtectedPage() {
 	const supabase = await createClient();
-	const { data: travels } = await supabase.from("travels").select().order("created_at", { ascending: false });
-
+	const { data: travels } = await supabase
+  .from("travels")
+  .select(`
+    travel_id,
+    travel_name,
+    created_by,
+    travel_members (
+      user_id,
+      role
+    )
+  `)
+  .order("created_at", { ascending: false });
 	const {
 		data: { user },
 	} = await supabase.auth.getUser();
+
+	console.log(travels);
 
 	if (!user) {
 		return redirect("/sign-in");
@@ -20,7 +32,9 @@ export default async function ProtectedPage() {
 			<CreateTravelDialog />
 			<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
 				{travels?.map((travel) => (
-					<TravelCard key={travel.id} travelId={travel.id} travelName={travel.travel_name} createdBy={travel.created_by} />
+					<TravelCard key={travel.travel_id} travelId={travel.travel_id} travelName={travel.travel_name} createdBy={travel.created_by}
+					travelMembers={travel.travel_members}
+					/>
 				))}
 			</div>
 		</div>
