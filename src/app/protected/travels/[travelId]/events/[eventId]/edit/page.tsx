9a2e5c3c7d5e722eeb06369ec4fa3ffae18a5019
hi mochizuki <hi.mochizuki@buysell-technologies.com>
2025-01-04 20:07:29 +0900

    feat: イベント編集画面の作成

---
+++ src/app/protected/travels/[travelId]/events/[eventId]/edit/page.tsx
@@ -0,0 +1,11 @@
+type Params = Promise<{
+  travelId: string;
+}>;
+
+export default async function NewTravelEventPage({ params }: { params: Params }) {
+  const {travelId} = await params;
+  return (
+    <div className="flex-1 w-full flex flex-col gap-12">
+    </div>
+  );
+}
