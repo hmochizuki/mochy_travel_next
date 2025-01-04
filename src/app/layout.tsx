d6a90f64a88f6faaf69bf33234520de08216925e
hi mochizuki <hi.mochizuki@buysell-technologies.com>
2025-01-04 18:39:31 +0900

    feat: レイアウとの微修正

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -44,7 +44,7 @@ export default function RootLayout({
 									<HeaderAuth />
 								</div>
 							</nav>
-							<div className="flex flex-col gap-20 max-w-5xl p-5">
+							<div className="flex flex-col gap-2 max-w-5xl p-5">
 								{children}
 							</div>
 						</div>
